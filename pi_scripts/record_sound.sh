fefcb4d2cd56e733d80bbff8d6b013a160d35941
Andrew McCafferty <andymccafferty@gmail.com>
2024-07-19 10:05:20 +0100

    Add scripts for Pi recording

---
+++ pi_scripts/record_sound.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+FILE_NAME=$(date +"%Y-%m-%dT%H-%M-%S").mp3
+OUTPUT_DIR=/path/to/output
+echo "Recording sound to ${FILE_NAME}" | systemd-cat
+ffmpeg -f alsa -i plughw:1 -t 30 ${OUTPUT_DIR}/temp_${FILE_NAME}
+mv ${OUTPUT_DIR}/temp_${FILE_NAME} ${OUTPUT_DIR}/final_${FILE_NAME}
