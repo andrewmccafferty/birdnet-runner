6ef93f257b1026446f754e27bc7e1b669ef13135
Andrew McCafferty <andymccafferty@gmail.com>
2024-07-19 12:45:51 +0100

    Add timestamp to Slack messages

--- send_slack_data.py
+++ send_slack_data.py
@@ -1,9 +1,24 @@
 import os
+import re
 
 from slack_sdk import WebClient
 
 from config import SLACK_BOT_TOKEN, SLACK_CHANNEL_ID, NO_SLACK_SEND, EXCLUDED_SPECIES
 
+
+def extract_date_from_filename(file_path):
+    # Define the regex pattern to match the ISO timestamp in the file name
+    pattern = r'(\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2})'
+
+    # Search for the pattern in the file path
+    match = re.search(pattern, file_path)
+
+    # If a match is found, return the timestamp with colons instead of hyphens for time parts
+    if match:
+        iso_timestamp = match.group(1).replace('-', ':', 2)
+        return iso_timestamp
+    else:
+        return None
 
 def _should_send_species_notification(species: str) -> bool:
     return species not in EXCLUDED_SPECIES
@@ -26,5 +41,5 @@ def send_bird_audio_file_to_slack(
         channel=SLACK_CHANNEL_ID,
         title=os.path.basename(file_path),
         file=file_path,
-        initial_comment=f"🐦 Possible {species} detected 🐦",
+        initial_comment=f"🐦 {species} detected at {extract_date_from_filename(file_path)} 🐦",
     )
